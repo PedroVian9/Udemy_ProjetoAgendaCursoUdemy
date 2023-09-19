f1d91f0c96b66150c77d01edc2bf069b9957657e
Pedro Macedo Paronetto Faria Viana <pedro.viana@gavresorts.com.br>
2023-09-19 16:58:47 -0300

    Começo de implementação do Script SQL

--- Agenda.DAL/Contatos.cs
+++ Agenda.DAL/Contatos.cs
@@ -3,6 +3,8 @@ using System.Data.SqlClient;
 using Agenda.Domain;
 using Microsoft.Extensions.Configuration;
 using System.IO;
+using Dapper;
+using System.Linq;
 
 namespace Agenda.DAL
 {
@@ -13,58 +15,36 @@ namespace Agenda.DAL
 
         public Contatos()
         {
-            IConfigurationRoot configuracao = new ConfigurationBuilder().SetBasePath(Directory.GetCurrentDirectory()).AddJsonFile("appsettings.json").Build();
-            //_strCon = @"Data Source=.\sqlexpress;Initial Catalog=Agenda;Integrated Security=True;";
+            IConfigurationRoot configuracao = new ConfigurationBuilder().SetBasePath(Directory.GetCurrentDirectory()).AddJsonFile("appsettings.json").Build();            
             _strCon = configuracao.GetConnectionString("con");
             _con = new SqlConnection(_strCon);
         }
 
         public void Adicionar(Contato contato)
         {
-            _con.Open();
-
-            var sql = String.Format("insert into Contato (Id, Nome) values ('{0}', '{1}');", contato.Id, contato.Nome);
-            var cmd = new SqlCommand(sql, _con);
-
-            cmd.ExecuteNonQuery();
-            _con.Close();
+            using (var con = new SqlConnection(_strCon))
+            {
+                con.Execute("insert into Contato (Id, Nome) values (@Id, @Nome)", contato);
+            }
         }
 
         public Contato Obter(Guid id)
         {
-            _con.Open();
-
-            var sql = String.Format("select Id, Nome from Contato where Id = '{0}';", id);
-            var cmd = new SqlCommand(sql, _con);
-
-            var sqlDataReader = cmd.ExecuteReader();
-            sqlDataReader.Read();
-            var contato = new Contato()
+            Contato contato;
+            using (var con = new SqlConnection(_strCon))
             {
-                Id = Guid.Parse(sqlDataReader["Id"].ToString()),
-                Nome = sqlDataReader["Nome"].ToString()
-            };
-
+                contato = con.QueryFirst<Contato>("select Id, Nome from Contato where Id = @Id", new { ID = id });   
+            }
             return contato;
         }
 
         public List<Contato> ObterTodos()
         {
             var contatos = new List<Contato>();
-            _con.Open();
 
-            var sql = String.Format("select Id, Nome from Contato;");
-            var cmd = new SqlCommand(sql, _con);
-
-            var sqlDataReader = cmd.ExecuteReader();
-            while(sqlDataReader.Read())
+            using (var con = new SqlConnection(_strCon))
             {
-                var contato = new Contato()
-                {
-                    Id = Guid.Parse(sqlDataReader["Id"].ToString()),
-                    Nome = sqlDataReader["Nome"].ToString()
-                };
-                contatos.Add(contato);
+                contatos = con.Query<Contato>("select Id, Nome from Contato").ToList();
             }
             return contatos;
         }
--- Test02/BaseTest.cs
+++ Test02/BaseTest.cs
@@ -5,7 +5,9 @@ using System.Text;
 using System.Threading.Tasks;
 using NUnit.Framework;
 using System.Configuration;
-
+using System.Runtime.Intrinsics.Arm;
+using System.Data.SqlClient;
+using System.IO;
 namespace Agenda.DAL.Test
 {
     [TestFixture]
@@ -17,22 +19,74 @@ namespace Agenda.DAL.Test
 
         public BaseTest()
         {
-            //_script = @"ProjetoDB_Create.sql";
-            //_con = ConfigurationManager.ConnectionStrings["conSetUpTest"].ConnectionString;
-            //_catalogTest = ConfigurationManager.ConnectionStrings["conSetUpTest"].ProviderName;
+            _script = @"ProjetoDB_Create.sql";
+            _con = ConfigurationManager.ConnectionStrings["conSetUpTest"].ConnectionString;
+            _catalogTest = ConfigurationManager.ConnectionStrings["conSetUpTest"].ProviderName;
         }
 
 
         [OneTimeSetUp]
         public void OneTimeSetup()
         {
-
+            CreateDBTest();
         }
 
         [OneTimeTearDown]
-        public void OneTimeTearDown() 
-        { 
+        public void OneTimeTearDown()
+        {
+            DeleteDBTest();
+        }
 
+        private void CreatDBTest()
+        {
+            using (var con = new SqlConnection(_con))
+            {
+                con.Open();
+                var scriptSql = File
+                    .ReadAllText($@"{AppDomain.CurrentDomain.SetupInformation.ApplicationBase}\{_script}")
+                    .Replace("$(DefaultDataPath)", $@"{AppDomain.CurrentDomain.SetupInformation.ApplicationBase}")
+                    .Replace("$(DefaultLogPath)", $@"{AppDomain.CurrentDomain.SetupInformation.ApplicationBase}")
+                    .Replace("$(DefaultFilePrefix)", _catalogTest)
+                    .Replace("$(DatabaseName)", _catalogTest)
+                    .Replace("WITH (DATA_COMPRESSION = PAGE)", string.Empty)
+                    .Replace("SET NOEXEC ON", string.Empty)
+                    .Replace("GO\r\n", "|");
+
+                ExecuteScriptSql(con, scriptSql);
+            }
+        }
+
+        private void ExecuteScriptSql(SqlConnection con, string scriptSql)
+        {
+            using (var cmd = con.CreateCommand())
+            {
+
+                foreach (var sql in scriptSql.Split('|'))
+                {
+                    cmd.CommandText = sql;
+                    try
+                    {
+                        cmd.ExecuteNonQuery();
+                    }
+                    catch (Exception e)
+                    {
+                        Console.WriteLine(sql);
+                        Console.WriteLine(e.Message);
+                    }
+                }
+            }
+        }
+
+        private void DeleteDBTest()
+        {
+            using (var con = new SqlConnection(_con))
+            {
+                con.Open();
+                using (var cmd = con.CreateCommand())
+                {
+                    cmd.CommandText = 
+                }
+            }    
         }
 
     }
